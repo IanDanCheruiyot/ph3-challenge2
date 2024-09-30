b1abd0fb0d7ef28942cec9739d15e9ce1a21810b
IanDanCheruiyot <daniank08@gmail.com>
2024-09-30 15:39:47 +0300

    order

--- order.py
+++ order.py
@@ -15,3 +15,23 @@ class Order:
         if not isinstance(value, Customer):
             raise ValueError("Order must have a valid Customer.")
         self._customer = value
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, value):
+        if not isinstance(value, Coffee):
+            raise ValueError("Order must have a valid Coffee.")
+        self._coffee = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)) or not (1.0 <= value <= 10.0):
+            raise ValueError("Price must be a float between 1.0 and 10.0.")
+        self._price = value
